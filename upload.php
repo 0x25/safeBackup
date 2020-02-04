84bf1ed2d8b8051d46086066d2ec3e9c35d74f89
john doe <0x25@users.noreply.github.com>
2020-02-04 13:59:05 +0100

    Update upload.php

--- upload.php
+++ upload.php
@@ -22,9 +22,10 @@ $ds = DIRECTORY_SEPARATOR;
 $pubKey = 'public.pem';
 $storeFolder = 'uploads';
 $log = 'out.txt';
-$db = 'db.txt';
 
 /** CODE **/
+
+//check if pubKey exist
 $cert = file_get_contents($pubKey);
 if( $cert === false){
 	file_put_contents($log,'ERROR : Fail to load RSA cert; check cert path'."\n",FILE_APPEND);
@@ -32,36 +33,36 @@ if( $cert === false){
 }
 
 $pk1 = openssl_get_publickey($cert);
+//check load of pubKey
 if($pk1 === false){
 	file_put_contents($log,'ERROR : Fail to extract pub cert; check cert format'."\n",FILE_APPEND);
 	exit(2);	
 }
 
+//check files
 if (!empty($_FILES)) {
-
+	//check upload error
 	if($_FILES['file']['error'] !== 0){
-
 		file_put_contents($log,'ERROR : Load file error ['.$_FILES['file']['error']."]\n",FILE_APPEND);
 		exit(3);
 	}
 
-    $mktime = str_replace('.','',microtime(true));
-    //file_put_contents($log,serialize($_FILES)."\n",FILE_APPEND);
+    $mktime = str_replace('.','',microtime(true)); 	//generate uniq value to suffixe file; maybe not the best idea
     $data = file_get_contents($_FILES['file']['tmp_name']);            
     unset($_FILES['file']['tmp_name']);
 
-    $targetPath = dirname( __FILE__ ) . $ds. $storeFolder . $ds;
-    $fileName = $_FILES['file']['name'].'.'.$mktime; //create uniq name
+    $targetPath = dirname( __FILE__ ) . $ds . $storeFolder . $ds;
+    $fileName = $_FILES['file']['name'].'.'.$mktime; //create uniq file name
     $targetFile =  $targetPath.$fileName;
  
-    file_put_contents($log,strlen($data)."\n",FILE_APPEND);
-	file_put_contents($log,$targetFile."\n",FILE_APPEND);
+    file_put_contents($log,'INFO : data length '.strlen($data)."\n",FILE_APPEND);
+	file_put_contents($log,'INFO : targetFile '.$targetFile."\n",FILE_APPEND);
     
 	$res = openssl_seal($data, $sealed, $ekeys, array($pk1));
 
 	if($res === false){
-		file_put_contents($log,'Fail seal data');
-		exit();	
+		file_put_contents($log,'ERROR : Fail seal data');
+		exit(4);	
 	}
 
 	file_put_contents($targetFile,base64_encode($ekeys[0])."\r\n");
